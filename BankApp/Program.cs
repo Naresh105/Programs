eb0e4621cc16d7bd26f4b0b06d9e8eacc1743c37
Naresh105 <nareshkuruva616@gmail.com>
2025-01-22 12:51:15 +0530

    Add files via upload

---
+++ BankApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("user details module");
+            Depositdll.Class1 obj= new Depositdll.Class1();
+            obj.Deposit();
+            Withdrawldl.Class1 obj2 = new Withdrawldl.Class1();
+            obj2.withdrawl();
+            Console.ReadLine(  );
+        }
+    }
+}
